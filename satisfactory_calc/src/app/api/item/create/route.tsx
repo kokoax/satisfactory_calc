8067c3f2de1c7f87f3464f5fe3b1f4b6e48684a1
kokoax <yomo4808@gmail.com>
2023-04-30 17:33:29 +0900

    itemをcreateできるようにした

--- satisfactory_calc/src/app/api/item/create/route.tsx
+++ satisfactory_calc/src/app/api/item/create/route.tsx
@@ -1,23 +1,15 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client';
 
-type paramsType = {
-  itemName: string;
-};
-
-type Props = {
-  params: paramsType
-};
-
 export async function POST(request: Request) {
-  const prisma = new PrismaClient({ log: [{emit: "stdout", level: "query"}, "info", "warn", "error"] });
+  const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
   const { itemName } = await request.json();
-  const data = { name: itemName };
 
-  console.log('start item create');
-  console.log(data);
-  prisma.item.create({ data });
-  console.log('end item create');
+  await prisma.item.create({data: {
+      name: itemName,
+    },
+  });
+
 
   return NextResponse.json(
     {
